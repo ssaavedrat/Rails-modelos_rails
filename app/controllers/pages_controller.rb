e968ef97ca87fa5bf8f8c95214dbd9b7760a4452
Sergio Saavedra <sergio.saavedra@ug.uchile.cl>
2023-07-26 18:36:35 -0400

    add fields age and email

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   def index; end
 
   def create
-    User.create(name: params[:name], age: params[:age])
+    User.create(name: params[:name], age: params[:age], email: params[:email])
   end
 end
